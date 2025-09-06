9b658c257732ec0721a17fcf2df52a5451147363
Feras Nawafleh <feras.a.nawafleh@gmail.com>
2025-09-06 18:20:59 +0400

    updated welcome page.

--- obj/Debug/net9.0/myWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/myWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94b4443bfdeaab4cc1382ff34db6a0cb3a529026")]
 [assembly: System.Reflection.AssemblyProductAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
