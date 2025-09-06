097bcd7bb981c4b949d341e8049fb55e11c7fa62
Feras Nawafleh <feras.a.nawafleh@gmail.com>
2025-09-06 18:26:25 +0400

    Feature 1 added to Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // HTTP Strict-Transport-Security response header (often abbreviated as HSTS)
     app.UseHsts();
 }
 
